8dc347c243be7e021eaea055417c8fdb825aa2f9
Giovanni Hernández <giovannihernandezm26@gmail.com>
2020-10-21 18:08:28 -0500

    Initial commit

---
+++ Kotlin-Tutorials-Kotlin-Tutorial-1-Source-code/KotlinTutorial/src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+import java.awt.Color.red
+
+fun main(args: Array<String>) {
+ /*   val lives = 3;
+
+    if (lives<1){
+        println("Game over!")
+    }*/
+
+    println("How old are you: ")
+    val ege = readLine();
+    println("Your ege is $ege")
+}
